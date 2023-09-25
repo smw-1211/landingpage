d91d77fe12365054e5de47c9104fac6e9cc84a1b
smw-1211 <wattamwarsoham2000@gmail.com>
2023-09-25 22:57:48 +0530

    Added a new section to landing page of testimonials.

---
+++ landingpage/src/components/LandingPage/testimonials.js
@@ -0,0 +1,159 @@
+import React from 'react'
+
+export default function Testimonials() {
+    return (
+      <div>
+        <section className="section-testimonials">
+        <div className="testimonials-container">
+          <span className="subheading">Testimonials</span>
+          <h2 className="heading-secondary">Once you try it, you can't go back</h2>
+
+          <div className="testimonials">
+            <figure className="testimonial">
+              <img
+                className="testimonial-img"
+                alt="Photo of customer Dave Bryson"
+                src="assets/img/customers/dave.jpg"
+              />
+              <blockquote className="testimonial-text">
+                Inexpensive, healthy and great-tasting meals, without even
+                having to order manually! It feels truly magical.
+              </blockquote>
+              <p className="testimonial-name">&mdash; Dave Bryson</p>
+            </figure>
+
+            <figure className="testimonial">
+              <img
+                className="testimonial-img"
+                alt="Photo of customer Ben Hadley"
+                src="assets/img/customers/ben.jpg"
+              />
+              <blockquote className="testimonial-text">
+                The AI algorithm is crazy good, it chooses the right meals for
+                me every time. It's amazing not to worry about food anymore!
+              </blockquote>
+              <p className="testimonial-name">&mdash; Ben Hadley</p>
+            </figure>
+
+            <figure className="testimonial">
+              <img
+                className="testimonial-img"
+                alt="Photo of customer Steve Miller"
+                src="assets/img/customers/steve.jpg"
+              />
+              <blockquote className="testimonial-text">
+                fitomee is a life saver! I just started a company, so there's
+                no time for cooking. I couldn't live without my daily meals now!
+              </blockquote>
+              <p className="testimonial-name">&mdash; Steve Miller</p>
+            </figure>
+
+            <figure className="testimonial">
+              <img
+                className="testimonial-img"
+                alt="Photo of customer Hannah Smith"
+                src="assets/img/customers/hannah.jpg"
+              />
+              <blockquote className="testimonial-text">
+                I got fitomee for the whole family, and it frees up so much
+                time! Plus, everything is organic and vegan and without plastic.
+              </blockquote>
+              <p className="testimonial-name">&mdash; Hannah Smith</p>
+            </figure>
+          </div>
+        </div>
+
+        <div className="gallery">
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-1.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-4.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-5.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-2.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-3.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-6.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-7.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-8.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-9.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-10.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-11.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+          <figure className="gallery-item">
+            <img
+              src="assets/img/gallery/gallery-12.jpg"
+              alt="Photo of beautifully
+            arranged food"
+            />
+          </figure>
+        </div>
+      </section>
+
+
+      </div>
+    )
+  }
+
+export { Testimonials }
--- landingpage/src/pages/Landingpage/LandingPage.jsx
+++ landingpage/src/pages/Landingpage/LandingPage.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/LandingPage/header';
 import Hero from '../../components/LandingPage/hero'
 import Info from '../../components/LandingPage/Info'
 import Index from '../../components/LandingPage/index'
+import Testimonials from '../../components/LandingPage/testimonials'
 
 export default function LandingPage() {
   return (
@@ -11,6 +12,7 @@ export default function LandingPage() {
       <Header />
       <Hero />
       <Info />
+      <Testimonials />
       <Index />
     </div>
   )
